3c80922dafeeb4038f23a1e5f316f00e7ac0de47
p3code <p3cod3@gmail.com>
2021-02-04 16:39:17 +0000

    ajuste cypress

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -2,5 +2,6 @@ const { createYield } = require("typescript");
 
 describe('Verificar se o projeto abriu', () => {
   it('Validar tela inicial', () => {
+    cy.visit('http://localhost:4200/');
   })
 });
